3a70585245bf693b017b53bbff2a60633ef7eff8
Chi Yeung <yeun0007@umn.edu>
2022-04-10 11:42:14 -0500

    Functional exercise

---
+++ app/src/main/java/com/ctyeung/collapsetoolbarex/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.ctyeung.collapsetoolbarex
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.AppBarLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mAppBarLayout: AppBarLayout = findViewById(R.id.app_bar) as AppBarLayout
+        mAppBarLayout.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
+            var isShow = false
+            var scrollRange = -1
+            override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange()
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    isShow = true
+                } else if (isShow) {
+                    isShow = false
+                }
+            }
+        })
+    }
+}
